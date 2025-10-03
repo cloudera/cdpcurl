bd33bea03100677220833543330780d150d20e97
Webster Mudge <wmudge@cloudera.com>
2025-10-03 14:51:52 -0400

    Update cryptography, build, test, and licensing (#6)
    * Remove legacy build artifacts
    * Update build tooling to use Hatch
    * Add pre-commit configuration
    * Relicense to Apache-2
    * Update tests to pytest and mock fixtures and Hatch matrices 
    * Refactor assertions and add tests for malformed headers
    * Update verbose logging
    * Add Trusted Publisher workflow
    * Add YAML and JSON checks
    
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- tests/stages_test.py
+++ tests/stages_test.py
@@ -1,81 +1,125 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2025 Cloudera, Inc.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Test cases for separate signature header calculation stages.
 """
-
-from unittest import TestCase
-
-__author__ = 'cloudera'
 
 from cdpcurl.cdpv1sign import (
     create_canonical_request_string,
     create_signature_string,
     create_encoded_authn_params_string,
-    create_signature_header)
+    create_signature_header,
+)
 
-class TestStages(TestCase):
+
+def test_create_canonical_request_string():
     """
-    Suite to test all stages.
+    Test the function to create the "canonical" request string.
     """
-    maxDiff = None
 
-    def test_create_canonical_request_string(self):
-        """
-        Test the function to create the "canonical" request string.
-        """
-        uri = 'https://cdpapitest.cloudera.com/api/v1/test/doTestThing'
-        headers = {'content-type': 'application/json',
-                   'x-altus-date': 'Tue, 3 Jun 2008 11:05:30 GMT'}
-        canonical_request_string = create_canonical_request_string(
-            'POST',
-            uri,
-            headers,
-            'ed25519v1')
-        self.assertEqual(canonical_request_string, "POST\n"
-                         "application/json\n"
-                         "Tue, 3 Jun 2008 11:05:30 GMT\n"
-                         "/api/v1/test/doTestThing\n"
-                         "ed25519v1")
+    uri = "https://cdpapitest.cloudera.com/api/v1/test/doTestThing"
+    headers = {
+        "content-type": "application/json",
+        "x-altus-date": "Tue, 3 Jun 2008 11:05:30 GMT",
+    }
 
-    def test_create_signature_string(self):
-        """
-        Test the function to sign and encode the canonical request string.
-        """
-        canonical_request_string = "POST\n"\
-            "application/json\n"\
-            "Tue, 3 Jun 2008 11:05:30 GMT\n"\
-            "/api/v1/test/doTestThing\n"\
-            "x-amz-date:20190921T022008Z\n"\
-            "ed25519v1"
-        private_key = 'Mzjg58S93/qdg0HuVP6PsLSRDTe+fQZ5++v/mkUUx4k='
-        signature_string = create_signature_string(
-            canonical_request_string,
-            private_key)
-        self.assertEqual(signature_string,
-                         'MujQ0wGS6CADCcidsqPvJ9_ETbK_RFUq9DQj3dcLPtj43xC46wjprhPQ_J3o9qFK8k1rhU3lOt7WbRSjCrGvDA==')
+    canonical_request_string = create_canonical_request_string(
+        "POST",
+        uri,
+        headers,
+        "ed25519v1",
+    )
+
+    expected = "\n".join(
+        [
+            "POST",
+            "application/json",
+            "Tue, 3 Jun 2008 11:05:30 GMT",
+            "/api/v1/test/doTestThing",
+            "ed25519v1",
+        ],
+    )
+
+    assert canonical_request_string == expected
 
 
-    def test_create_encoded_authn_params_string(self):
-        """
-        Test the function to create the encoded authentication parameters
-        string.
-        """
-        access_key = 'ABC';
-        auth_method = 'ed25519v1'
-        encoded_auth_params_string = create_encoded_authn_params_string(
-            access_key,
-            auth_method)
-        self.assertEqual(encoded_auth_params_string,
-                         b'eyJhY2Nlc3Nfa2V5X2lkIjogIkFCQyIsICJhdXRoX21ldGhvZCI6ICJlZDI1NTE5djEifQ==')
+def test_create_signature_string():
+    """
+    Test the function to sign and encode the canonical request string.
+    """
 
-    def test_create_signature_header(self):
-        """
-        Test the function that generates the signature header value.
-        """
-        encoded_auth_params_string = b'eyJhY2Nlc3Nfa2V5X2lkIjogIkFCQyIsICJhdXRoX21ldGhvZCI6ICJlZDI1NTE5djEifQ=='
-        signature = 'MujQ0wGS6CADCcidsqPvJ9_ETbK_RFUq9DQj3dcLPtj43xC46wjprhPQ_J3o9qFK8k1rhU3lOt7WbRSjCrGvDA=='
-        signature_header = create_signature_header(
-            encoded_auth_params_string,
-            signature)
-        self.assertEqual(signature_header,
-                         'eyJhY2Nlc3Nfa2V5X2lkIjogIkFCQyIsICJhdXRoX21ldGhvZCI6ICJlZDI1NTE5djEifQ==.MujQ0wGS6CADCcidsqPvJ9_ETbK_RFUq9DQj3dcLPtj43xC46wjprhPQ_J3o9qFK8k1rhU3lOt7WbRSjCrGvDA==')
+    canonical_request_string = (
+        "POST\n"
+        "application/json\n"
+        "Tue, 3 Jun 2008 11:05:30 GMT\n"
+        "/api/v1/test/doTestThing\n"
+        "x-amz-date:20190921T022008Z\n"
+        "ed25519v1"
+    )
+    private_key = "Mzjg58S93/qdg0HuVP6PsLSRDTe+fQZ5++v/mkUUx4k="
+
+    signature_string = create_signature_string(
+        canonical_request_string,
+        private_key,
+    )
+
+    assert (
+        signature_string
+        == "MujQ0wGS6CADCcidsqPvJ9_ETbK_RFUq9DQj3dcLPtj43xC46wjprhPQ_J3o9qFK8k1rhU3lOt7WbRSjCrGvDA=="
+    )
+
+
+def test_create_encoded_authn_params_string():
+    """
+    Test the function to create the encoded authentication parameters
+    string.
+    """
+
+    access_key = "ABC"
+    auth_method = "ed25519v1"
+
+    encoded_auth_params_string = create_encoded_authn_params_string(
+        access_key,
+        auth_method,
+    )
+
+    assert (
+        encoded_auth_params_string
+        == b"eyJhY2Nlc3Nfa2V5X2lkIjogIkFCQyIsICJhdXRoX21ldGhvZCI6ICJlZDI1NTE5djEifQ=="
+    )
+
+
+def test_create_signature_header():
+    """
+    Test the function that generates the signature header value.
+    """
+
+    encoded_auth_params_string = (
+        b"eyJhY2Nlc3Nfa2V5X2lkIjogIkFCQyIsICJhdXRoX21ldGhvZCI6ICJlZDI1NTE5djEifQ=="
+    )
+    signature = "MujQ0wGS6CADCcidsqPvJ9_ETbK_RFUq9DQj3dcLPtj43xC46wjprhPQ_J3o9qFK8k1rhU3lOt7WbRSjCrGvDA=="
+
+    signature_header = create_signature_header(
+        encoded_auth_params_string,
+        signature,
+    )
+
+    assert (
+        signature_header
+        == "eyJhY2Nlc3Nfa2V5X2lkIjogIkFCQyIsICJhdXRoX21ldGhvZCI6ICJlZDI1NTE5djEifQ==.MujQ0wGS6CADCcidsqPvJ9_ETbK_RFUq9DQj3dcLPtj43xC46wjprhPQ_J3o9qFK8k1rhU3lOt7WbRSjCrGvDA=="
+    )
